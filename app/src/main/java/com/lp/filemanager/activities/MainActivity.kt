070f7d74133a8661c973eec7b922bf504b88e07f
lp <1079107009@qq.com>
2017-09-19 15:31:41 +0800

    搭建项目

---
+++ app/src/main/java/com/lp/filemanager/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.lp.filemanager.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.lp.filemanager.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ app/src/main/java/com/lp/filemanager/application/FileApplication.kt
@@ -0,0 +1,20 @@
+package com.lp.filemanager.application
+
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+/**
+ * Created by LiPin on 2017/9/19 13:52.
+ * 描述：
+ */
+class FileApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            //这个进程是专门LeakCanary用于堆分析的，不应该在这个进程初始化app
+            return
+        }
+        LeakCanary.install(this)
+    }
+}
